d2d4f84dc6eab9c8a90233c3dca018c2aa41dfdf
shanmukh0504 <shanmukh0504@gmail.com>
2025-01-28 11:50:54 +0530

    updated imports

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect } from "react";
 import { useAccount } from "wagmi";
 import { useRouter } from "next/navigation";
-
 import TokenSwap from "../components/swap/TokenSwap";
 import Transactions from "../components/transactions/Transactions";
 
